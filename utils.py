946c9d316f7ff6464c30e12714890a84796360b8
Kirill Litoshko <kovzik08@gmail.com>
2023-04-19 14:26:48 +0300

    updated utils (application pre start and post finish functions)

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import functools
 import json
 
-from settings import SETTING_PATH
+from settings import TEST_CONFIG
+from sqlalchemy.ext.asyncio import create_async_engine
 from yaml import safe_load
 
 
 def get_app_config():
-    with open(SETTING_PATH) as file:
+    with open(TEST_CONFIG) as file:
         config = safe_load(file)
     return config
 
@@ -14,6 +15,17 @@ def get_app_config():
 def construct_db_url(config):
     dsn = "postgresql+asyncpg://{username}:{password}@{host}:{port}/{database}"
     return dsn.format(**config)
+
+
+async def on_startup(app):
+    application_name = app["app_name"]
+    config = get_app_config()
+    db_url = construct_db_url(config[application_name])
+    app['db'] = create_async_engine(db_url, echo=True)
+
+
+async def on_shutdown(app):
+    await app["db"].dispose()
 
 
 pretty_json = functools.partial(
