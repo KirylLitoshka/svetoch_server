e66ae75a0c0aefce8225f9a9fcf481ccc66bc7b1
Kirill Litoshko <kovzik08@gmail.com>
2023-04-20 14:24:29 +0300

    added area model

---
+++ electricity/models.py
@@ -0,0 +1,13 @@
+import sqlalchemy as sa
+
+__all__ = [
+    "metadata", "areas"
+]
+
+metadata = sa.MetaData()
+
+areas = sa.Table(
+    "areas", metadata,
+    sa.Column("id", sa.Integer, primary_key=True),
+    sa.Column("title", sa.String, nullable=False, unique=True)
+)
