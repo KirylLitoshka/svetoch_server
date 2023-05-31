aa234779b638d5bfc7a8899903bc9b0bad48c454
Kiryl Litoshka <kirill.litoshko@gmail.com>
2023-05-31 14:53:58 +0300

    added views for objects and objects meters

--- electricity/views.py
+++ electricity/views.py
@@ -1,5 +1,8 @@
+import asyncio
+from datetime import datetime
 from electricity.models import *
-from sqlalchemy.sql import and_, func, select, text
+from sqlalchemy.sql import and_, func, select, desc
+from sqlalchemy.sql.expression import literal_column
 from views import DetailView, ListView, pretty_json, web
 
 __all__ = [
@@ -7,7 +10,9 @@ __all__ = [
     "CiphersListView", "CipherDetailView",
     "RatesListView", "RateDetailView",
     "MetersListView", "MeterDetailView",
-    "WorkshopsListView", "WorkshopDetailView"
+    "WorkshopsListView", "WorkshopDetailView",
+    "ObjectsListView", "ObjectDetailView",
+    "ObjectMeterDetailView", "ObjectMetersListView"
 ]
 
 
@@ -48,7 +53,7 @@ class RatesListView(ListView):
                 ).order_by(rates.c.id)
             )
             result = [dict(row) for row in cursor.fetchall()]
-            return web.json_response({"status": "success", "items": result}, dumps=pretty_json)
+            return web.json_response({"success": True, "items": result}, dumps=pretty_json)
 
 
 class RateDetailView(DetailView):
@@ -69,3 +74,122 @@ class WorkshopsListView(ListView):
 
 class WorkshopDetailView(DetailView):
     model = workshops
+
+
+class ObjectsListView(ListView):
+    model = objects
+
+    async def get(self):
+        async with self.request.app['db'].connect() as conn:
+            object_meters_keys = [
+                object_meters.c[key] for key in object_meters.c.keys() if key != "meter_id"
+            ]
+            last_objects_meters_ids = select(func.max(object_meters.c.id).label("id")).group_by(
+                object_meters.c.object_id).order_by("id")
+            last_objects_meters = select(
+                *object_meters_keys, func.row_to_json(meters.table_valued()).label("type")
+            ).where(object_meters.c.id.in_(last_objects_meters_ids)).select_from(
+                object_meters.join(meters, isouter=True)
+            ).alias("meter")
+            object_keys = [
+                objects.c[key] for key in objects.columns.keys() if key not in ["area_id", "cipher_id"]
+            ]
+            smtm = select(
+                *object_keys,
+                func.row_to_json(ciphers.table_valued()).label("cipher"),
+                func.row_to_json(areas.table_valued()).label("area"),
+                func.row_to_json(
+                    literal_column(last_objects_meters.name)
+                ).label("meter")
+            ).select_from(
+                objects.join(ciphers).join(areas).join(last_objects_meters, isouter=True)
+            ).order_by(objects.c.id)
+
+            cursor = await conn.execute(smtm)
+            result = [dict(row) for row in cursor.fetchall()]
+            return web.json_response({"success": True, "items": result}, dumps=pretty_json)
+
+    async def post(self):
+        post_data = await self.request.json()
+        async with self.request.app['db'].begin() as conn:
+            cursor = await conn.execute(
+                objects.insert().returning(literal_column('*')).values(**post_data)
+            )
+            result = dict(cursor.fetchone())
+            return web.json_response({"success": True, "item": result}, dumps=pretty_json)
+
+
+class ObjectDetailView(DetailView):
+    model = objects
+
+
+class ObjectMetersListView(ListView):
+    model = object_meters
+
+    async def get(self):
+        object_id = int(self.request.match_info['object_id'])
+        async with self.request.app['db'].connect() as conn:
+            cursor = await conn.execute(
+                select(object_meters).where(
+                    object_meters.c.object_id == object_id
+                )
+            )
+            result = [dict(row) for row in cursor.fetchall()]
+            return web.json_response({"success": True, "items": result}, dumps=pretty_json)
+        
+    async def post(self):
+        object_id = int(self.request.match_info['object_id'])
+        request_data = await self.request.json()
+        if request_data['installation_date'] is not None:
+            request_data['installation_date'] = datetime.strptime(
+                request_data['installation_date'], "%Y-%m-%d"
+            ).date()
+        async with self.request.app['db'].begin() as conn:
+            await conn.execute(
+                self.model.insert().values(
+                    object_id = object_id,
+                    **request_data
+                )
+            )
+            return web.json_response({"success": True})
+
+
+
+class ObjectMeterDetailView(DetailView):
+    model = object_meters
+
+    async def get(self):
+        object_id = int(self.request.match_info['object_id'])
+        is_current = self.request.rel_url.path.split("/")[-1] == "current"
+        async with self.request.app['db'].connect() as conn:
+            if not is_current:
+                object_meter_id = int(self.request.match_info['id'])
+                query = select(object_meters).where(and_(
+                    object_meters.c.object_id == object_id,
+                    object_meters.c.id == object_meter_id
+                ))
+            else:
+                query = select(object_meters).where(
+                    object_meters.c.object_id == object_id
+                ).order_by(desc(object_meters.c.id)).limit(1)
+            cursor = await conn.execute(query)
+            object_meter_item = cursor.fetchone()
+            if not object_meter_item:
+                return web.json_response({"success": False, "reason": "Не найдено"}, dumps=pretty_json)
+            result = dict(object_meter_item)
+            return web.json_response({"success": True, "item": result}, dumps=pretty_json)
+
+    async def patch(self):
+        object_meter_id = int(self.request.match_info['id'])
+        request_data = await self.request.json()
+        if request_data['installation_date'] is not None:
+            request_data['installation_date'] = datetime.strptime(
+                request_data['installation_date'], "%Y-%m-%d"
+            ).date()
+        async with self.request.app['db'].begin() as conn:
+            await conn.execute(
+                object_meters.update().where(
+                    object_meters.c.id == object_meter_id
+                ).values(**request_data)
+            )
+            return web.json_response({"success": True})
